1b0dcda2f7410757d5e4cfc5a1be501ad1162873
alessandros0tgiu <alessandrosotgiu1206@gmail.com>
2026-08-28 14:09:14 +0200

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { WeatherEffects } from "./WeatherEffects";
 import { WeatherChart } from "./WeatherChart";
